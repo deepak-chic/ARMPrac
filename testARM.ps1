a5feedcefbc4ca339f88353c1213c3f73ee4c7d6
Deepak <Deepak@SEZLAP-10438.india.rsystems.com>
2024-02-08 19:52:28 +0530

    Production changes

--- testARM.ps1
+++ testARM.ps1
@@ -1,17 +1,44 @@
 $environment = "dev"
 $globalParameterFile = "./global-parameters.$environment.json"
 $rgInfraName = "rg-z-cplus-infra-n-001"
+$sshKeyName = "ssh-z-cplus-cus-db-n-001"
 
-# $templateFile = "./LoadBalancer/lb-template.json"
-# #$templateParameterFile = "./VirtualNetwork/vnet-template-parameters.$environment.json"
-# az deployment group create `
-#    --resource-group $rgInfraName `
-#    --template-file $templateFile `
-#    --parameters $globalParameterFile
+$templateFile = "./VirtualMachine/Db/vm-orad-template.json"
+$templateParameterFile = "./VirtualMachine/Db/vm-orad-template-parameters.$environment.json"
+az deployment group create `
+   --resource-group $rgInfraName `
+   --template-file $templateFile `
+   --parameters $globalParameterFile $templateParameterFile infraResourceGroupName=$rgInfraName sshKeyName=$sshKeyName
 
-$customers = @( @{'name'='123';"size" = "098"}, @{'name'='456';"size" = "056"})
+# Deploy the SQL DB VM
+$templateFile = "./VirtualMachine/Db/vm-sql-template.json"
+$templateParameterFile = "./VirtualMachine/Db/vm-sql-template-parameters.$environment.json"
+az deployment group create `
+   --resource-group $rgInfraName `
+   --template-file $templateFile `
+   --parameters $globalParameterFile $templateParameterFile infraResourceGroupName=$rgInfraName
 
-foreach($cust in $customers){
-   echo $cust.name
-   echo $cust.size
-}
+
+# Deploy the AI reciver VM
+$templateFile = "./VirtualMachine/Linux/vm-aire-template.json"
+$templateParameterFile = "./VirtualMachine/Linux/vm-aire-template-parameters.$environment.json"
+az deployment group create `
+   --resource-group $rgInfraName `
+   --template-file $templateFile `
+   --parameters $globalParameterFile $templateParameterFile infraResourceGroupName=$rgInfraName sshKeyName=$sshKeyName
+
+# Deploy the ProAct service VM
+$templateFile = "./VirtualMachine/Linux/vm-aise-template.json"
+$templateParameterFile = "./VirtualMachine/Linux/vm-aise-template-parameters.$environment.json"
+az deployment group create `
+   --resource-group $rgInfraName `
+   --template-file $templateFile `
+   --parameters $globalParameterFile $templateParameterFile infraResourceGroupName=$rgInfraName sshKeyName=$sshKeyName
+
+# Deploy the redis VM
+$templateFile = "./VirtualMachine/Linux/vm-redis-template.json"
+$templateParameterFile = "./VirtualMachine/Linux/vm-redis-template-parameters.$environment.json"
+az deployment group create `
+   --resource-group $rgInfraName `
+   --template-file $templateFile `
+   --parameters $globalParameterFile $templateParameterFile infraResourceGroupName=$rgInfraName sshKeyName=$sshKeyName
