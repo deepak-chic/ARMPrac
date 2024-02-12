f8205e81888f04901c38e44a4b72a971cd38a2cd
Deepak <Deepak@SEZLAP-10438.india.rsystems.com>
2024-02-12 19:05:30 +0530

    production ready code

--- testARM.ps1
+++ testARM.ps1
@@ -5,9 +5,6 @@ $rgInfraName = "rg-z-cplus-infra-p-001"
 
 # DB Resource Group and Resources
 
-$rgDBName = "rg-z-cplus-db-p-001"
-$sshKeyName = 'ssh-z-cplus-cus-db-p-001'
-
 # # Deploy the SQL DB VM
 # $templateFile = "./VirtualMachine/Db/vm-sql-template.json"
 # $templateParameterFile = "./VirtualMachine/Db/vm-sql-template-parameters.$environment.json"
@@ -16,22 +13,12 @@ $sshKeyName = 'ssh-z-cplus-cus-db-p-001'
 #    --template-file $templateFile `
 #    --parameters $globalParameterFile $templateParameterFile infraResourceGroupName=$rgInfraName
 
+$rgHubName = "rg-z-cplus-gw-p-001"
 
-$templateFile = "./VirtualMachine/Db/vm-sql-template.json"
-$templateParameterFile = "./VirtualMachine/Db/vm-sql-template-parameters.$environment.json"
-$vmSizes = @('Standard_E8as_v5', 'Standard_E8as_v5', 'Standard_E8as_v5', 'Standard_E20as_v5', 'Standard_E20as_v5', 'Standard_E8as_v5')
-$noOfInstance = $vmSizes.count
-$counter = 1
-while($counter -le $noOfInstance)
-{
-   $vmName = "vm-z-sqld-p-$($counter.ToString('000'))"
-   $nicName = "$vmName-nic"
-   
-   az deployment group create `
-   --resource-group $rgDBName `
+# Deploy the Application gateway
+$templateFile = "./APIManagementService/apim-template.json"
+$templateParameterFile = "./APIManagementService/apim-template-parameters.$environment.json"
+az deployment group create `
+   --resource-group $rgHubName `
    --template-file $templateFile `
-   --parameters $globalParameterFile $templateParameterFile infraResourceGroupName=$rgInfraName `
-   virtualMachineName=$vmName vmSize=$($vmSizes[$counter-1]) networkInterfacesName=$nicName
-
-   $counter +=1
-}
+   --parameters $globalParameterFile $templateParameterFile
